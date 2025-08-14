3ea120ff17801fa580b214e3ecddbdcafe99b9fe
Demigod122111 <simonsmithcampion9@gmail.com>
2025-08-13 22:28:04 -0500

    Globally Uncommented Development Update

--- push.bat
+++ push.bat
@@ -2,5 +2,5 @@ git init
 git add .
 git commit -m "Globally Uncommented Development Update"
 git branch -M main
-git remote add origin https://github.com/cctechteam/Tech-Media-Hub.git
+git remote add origin git@github.com:cctechteam/Tech-Media-Hub.git
 git push -u origin main
